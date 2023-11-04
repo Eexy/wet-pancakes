b160d540489a0880260ea3ad8e16cd3db04e5614
Boussif Isai <eexdhe@gmail.com>
2023-11-04 22:32:51 +0100

    chore: add gap between nav item and set layout for app

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 import { Nav } from "./shared/components/nav/nav.tsx";
 
 function App() {
-  return <div className={"min-h-screen"}>
+  return <div className={"min-h-screen flex flex-col md:grid md:grid-cols-8 lg:grid-cols-11 lg:gap-3"}>
     <Nav />
     <Outlet></Outlet>
   </div>;
--- src/shared/components/nav/nav.tsx
+++ src/shared/components/nav/nav.tsx
@@ -1,7 +1,7 @@
 import { NavItem } from "./nav-item.tsx";
 
 export function Nav() {
-  return <ul>
+  return <ul className={"flex flex-col gap-2"}>
     <NavItem to={"/"} name={"home"} />
     <NavItem to={"/watchlist"} name={"watchlist"} />
   </ul>;
