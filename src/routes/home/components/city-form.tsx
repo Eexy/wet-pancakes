0b87208403eee232341d94ce8b676f8dbf3459d6
Isai Boussif <eexdhe@gmail.com>
2023-11-06 12:19:58 +0100

    feat: add design for city form

--- src/routes/home/components/city-form.tsx
+++ src/routes/home/components/city-form.tsx
@@ -1,9 +1,10 @@
 import { Form } from "react-router-dom";
+import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
 
 export function CityForm() {
-  return <Form role={"search"}>
-    <label htmlFor={"city"}>City :</label>
-    <input type={"search"} id={"city"} name={"city"} />
-    <button type={"submit"}>Submit</button>
+  return <Form role={"search"}
+               className={"flex gap-3 p-1 border border-slate-400/80 shadow-sm rounded w-fit text-slate-400"}>
+    <MagnifyingGlassIcon className={"h-6 w-6 "} />
+    <input type={"search"} id={"city"} name={"city"} placeholder={"City"} className={"outline-none w-60"} />
   </Form>;
 }
