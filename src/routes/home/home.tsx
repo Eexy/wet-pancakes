a4c598b8df52a08e61bc4bc441d141852d3383a8
Isai Boussif <eexdhe@gmail.com>
2023-11-06 16:34:45 +0100

    feat: set basic flex style on Home component

--- src/routes/home/home.tsx
+++ src/routes/home/home.tsx
@@ -1,5 +1,4 @@
 import { SearchForm } from "./components/search-form.tsx";
-import { useLoaderData } from "react-router-dom";
 import { Location } from "../../types/location.ts";
 import { Forecast } from "../../types/forecast.ts";
 import { CurrentForecast } from "./components/current-forecast.tsx";
@@ -7,11 +6,7 @@ import { CurrentForecast } from "./components/current-forecast.tsx";
 export type HomeData = { location: Location, forecast: Forecast } | null
 
 export function Home() {
-  const res = useLoaderData() as HomeData;
-  console.info(res);
-
-
-  return <div className={"p-3"}>
+  return <div className={"p-3 flex flex-col gap-4"}>
     <SearchForm />
     <CurrentForecast />
   </div>;
