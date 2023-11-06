422afdc9ca170fd540a91587c74b12c7c9403da0
Isai Boussif <eexdhe@gmail.com>
2023-11-06 12:04:45 +0100

    refactor: put all sidebar related content in same directory

--- src/shared/layout/sidebar/sidebar.tsx
+++ src/shared/layout/sidebar/sidebar.tsx
@@ -1,4 +1,4 @@
-import { Nav } from "../components/nav/nav.tsx";
+import { Nav } from "./nav/nav.tsx";
 
 export function Sidebar() {
   return <div className={"hidden md:block md:col-span-2 border-r border-r-neutral-300 p-3"}>
