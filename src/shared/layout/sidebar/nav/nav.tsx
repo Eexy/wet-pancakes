924bfa3a52293607e769bcf0342ea00609ecebd3
Isai Boussif <eexdhe@gmail.com>
2023-11-06 12:07:49 +0100

    chore: increase padding

--- src/shared/layout/sidebar/nav/nav.tsx
+++ src/shared/layout/sidebar/nav/nav.tsx
@@ -2,7 +2,7 @@ import { NavItem } from "./nav-item.tsx";
 import { HeartIcon, RectangleGroupIcon } from "@heroicons/react/24/solid";
 
 export function Nav() {
-  return <ul className={"flex flex-col gap-2 items-stretch"}>
+  return <ul className={"flex flex-col gap-3 items-stretch"}>
     <NavItem to={"/"} name={"home"} icon={<RectangleGroupIcon />} />
     <NavItem to={"/watchlist"} name={"watchlist"} icon={<HeartIcon />} />
   </ul>;
--- src/shared/layout/sidebar/sidebar.tsx
+++ src/shared/layout/sidebar/sidebar.tsx
@@ -1,7 +1,7 @@
 import { Nav } from "./nav/nav.tsx";
 
 export function Sidebar() {
-  return <div className={"hidden md:block md:col-span-2 border-r border-r-neutral-300 p-3"}>
+  return <div className={"hidden md:block md:col-span-2 border-r border-r-neutral-300 px-3 py-5"}>
     <Nav />
   </div>;
 }
