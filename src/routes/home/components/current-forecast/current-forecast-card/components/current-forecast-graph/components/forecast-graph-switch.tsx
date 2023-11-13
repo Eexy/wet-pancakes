6ce4521a8b9a80a6ac04363dbb2baefb7dfe1361
Boussif Isai <eexdhe@gmail.com>
2023-11-13 14:31:26 +0100

    fix: use text-sky-950 by default in graph switch to get good tailwind to import all good style

--- src/routes/home/components/current-forecast/current-forecast-card/components/current-forecast-graph/components/forecast-graph-switch.tsx
+++ src/routes/home/components/current-forecast/current-forecast-card/components/current-forecast-graph/components/forecast-graph-switch.tsx
@@ -4,14 +4,14 @@ export type GraphSwitchProps = {
 }
 
 export function ForecastGraphSwitch({ type, setType }: GraphSwitchProps) {
-  return <div className={"overflow-hidden flex rounded-md bg-sky-950 w-fit"}>
+  return <div className={"overflow-hidden flex rounded-md bg-sky-950 w-fit text-sky-950"}>
     <button
-      className={`${type === "temp" ? "bg-white text-sky-950" : "bg-transparent"} text-white font-semibold py-1 px-3`}
+      className={`${type === "temp" ? "bg-white " : "bg-transparent text-white"}  font-semibold py-1 px-3`}
       onClick={() => setType("temp")}>
       Temperature
     </button>
     <button onClick={() => setType("humidity")}
-            className={`${type === "humidity" ? "bg-white text-sky-950" : "bg-transparent"} text-white font-semibold py-1 px-3`}>
+            className={`${type === "humidity" ? "bg-white " : "bg-transparent text-white"}  font-semibold py-1 px-3`}>
       Humidity
     </button>
   </div>;
